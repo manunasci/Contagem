4428b12382c202404d39faeb22c714fb4e3afcb5
UlissesSilva08 <ulissesmasterchief@gmail.com>
2021-08-27 20:44:14 -0300

    Programa exibe a conta regressiva

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Contagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparar...");
+            Console.ReadKey();
+
+            Console.WriteLine("Apontar...");
+            Console.ReadKey();
+
+            Console.WriteLine("FOGO!");
+        }
+    }
+}
